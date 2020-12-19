808eb46f03db7b84a0c286f29611323532a72858
Mauro Pinheiro <mauro.pcsergio@gmail.com>
2020-12-19 06:22:01 -0300

    php images update

--- build.sh
+++ build.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-export SSHPRI=$(cat ~/.ssh/id_rsa)
-export SSHPUB=$(cat ~/.ssh/id_rsa.pub)
-export GITGLO=$(cat ~/.gitconfig)
-
 docker-compose build
-
-unset SSHPRI
-unset SSHPUB
-unset GITGLO
